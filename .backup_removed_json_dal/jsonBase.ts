27b9b185f035153a3ca5a6c9ab53ef3b2a23ae53
Idan Tepper <idantepper@gmail.com>
2026-01-10 17:40:40 +0200

    chore: remove backup JSON DAL files

--- .backup_removed_json_dal/jsonBase.ts
+++
@@ -1,6 +0,0 @@
-// JSON-file DAL has been deprecated in this repository. Use Mongo DAL instead.
-export default class JsonBase<T extends { id: string }> {
-  constructor(..._args: any[]) {
-    throw new Error('JsonBase has been removed. Use Mongo DAL implementations instead.');
-  }
-}
