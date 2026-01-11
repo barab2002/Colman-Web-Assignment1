e1653e1f69143c7d62728b6d0a52fbdf201635b1
barab2002 <barab2002@gmail.com>
2026-01-11 20:45:37 +0200

    add html report

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,6 @@ module.exports = {
   },
   reporters: [
     'default',
-    [
-      'jest-html-reporters',
-      {
-        publicPath: './html-report',
-        filename: 'report.html',
-        expand: true,
-        openReport: true,
-      },
-    ],
+    ['jest-html-reporters', { openReport: true }],
   ],
 };
